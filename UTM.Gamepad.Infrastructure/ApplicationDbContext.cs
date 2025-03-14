65916cc7fb9c2533155dd0922e085343df907c81
Anton Brega <anton.brega@isa.utm.md>
2025-03-14 23:33:23 +0200

    Добавил контекст и строку подключения

---
+++ UTM.Gamepad.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,54 @@
+﻿using System.Data.Entity;
+using UTM.Gamepad.Domain;
+
+namespace UTM.Gamepad.Infrastructure
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() 
+            : base("GameStoreDb")
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Order>()
+                .HasRequired(o => o.User)
+                .WithMany(u => u.Orders)
+                .HasForeignKey(o => o.UserId)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Review>()
+                .HasRequired(r => r.User)
+                .WithMany(u => u.Reviews)
+                .HasForeignKey(r => r.UserId)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Review>()
+                .HasRequired(r => r.Product)
+                .WithMany(p => p.Reviews)
+                .HasForeignKey(r => r.ProductId)
+                .WillCascadeOnDelete(true);
+
+            modelBuilder.Entity<OrderItem>()
+                .HasRequired(oi => oi.Order)
+                .WithMany(o => o.OrderItems)
+                .HasForeignKey(oi => oi.OrderId)
+                .WillCascadeOnDelete(true);
+
+            modelBuilder.Entity<OrderItem>()
+                .HasRequired(oi => oi.Product)
+                .WithMany(p => p.OrderItems)
+                .HasForeignKey(oi => oi.ProductId)
+                .WillCascadeOnDelete(false);
+        }
+    }
+}
